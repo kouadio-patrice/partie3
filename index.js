3dee2b50e0e40c35f6bbf8ced4c549df3d0bb5aa
kouadio-patrice <koukpatrice@gmailcom>
2021-01-18 09:17:17 +0100

    3.3: Étape 3 du backend du répertoire

--- index.js
+++ index.js
@@ -1,18 +1,54 @@
-const url = require('url')
+//const url = require('url')
 const express = require('express')
-const { json } = require('express')
+//const { json } = require('express')
 const app = express()
 
-let info = [
-    {   
-        id:1, 
-        name: "phonebook  has for 4 people",
-        number:"04595612",
-        } 
-    
-] 
+let persons = [
+        { 
+          "name": "Arto Hellas", 
+          "number": "040-123456",
+          "id": 1
+        },
+        { 
+          "name": "Ada Lovelace", 
+          "number": "39-44-5323523",
+          "id": 2
+        },
+        { 
+          "name": "Dan Abramov", 
+          "number": "12-43-234345",
+          "id": 3
+        },
+        { 
+          "name": "Mary Poppendieck", 
+          "number": "39-23-6423122",
+          "id": 4
+        }
+      ]
+      app.get('/',(request,response) => {
+        response.send('<h1> Hello Word !</h1>')
+    })
+  
 
-  app.get('/',(request,response) => {
+    app.get('/api/persons/:id', (request , response) => {
+      const id = Number(request.params.id)
+      
+      const person = persons.find(person => person.id === id)
+
+      if(person){
+        response.json(person);
+      } else{
+        response.status(404).end()
+      }
+       
+    })
+     const PORT = 3001
+     app.listen(PORT,() => {
+         console.log(`server runing on port ${PORT}`)
+     } )
+     
+
+ /* app.get('/',(request,response) => {
       response.send('<h1> Hello Word !</h1>')
   })
 
@@ -34,5 +70,5 @@ let info = [
    const PORT = 3003 
    app.listen(PORT,() => {
        console.log(`server runing on port ${PORT}`)
-   } )
+   } )*/
    
